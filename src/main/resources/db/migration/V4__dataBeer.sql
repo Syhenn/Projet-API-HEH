6ecddc65cef75642b5f72544fc308723509fdd31
Cocoricodai <colin.depelsenaire@gmail.com>
2022-11-28 16:42:49 +0100

    Update DataBeer

--- src/main/resources/db/migration/V4__dataBeer.sql
+++ src/main/resources/db/migration/V4__dataBeer.sql
@@ -1,2 +1,5 @@
-insert into beers(nameBeer,url, degrees)  values ('Jupiler','http://jupiler.png', 5.2);
-insert into beers(nameBeer,url, degrees)  values ('Kaastelllll','http://Kaastelllll.png', 8);
+insert into beers(nameBeer,url, degrees)  values ('Jupiler','https://i.imgur.com/X0IdkBk.jpeg', 5.2);
+insert into beers(nameBeer,url, degrees)  values ('Kasteel','https://i.imgur.com/MwEd5rJ.jpeg', 8);
+insert into beers(nameBeer,url, degrees)  values ('Bière du pays','https://i.imgur.com/GQiEm7c.jpeg', 4);
+insert into beers(nameBeer,url, degrees)  values ('Grimbergen blonde','https://i.imgur.com/GOikUeu.jpeg', 6.7);
+insert into beers(nameBeer,url, degrees)  values ('Biere nouvelle','https://i.imgur.com/lnC1Vne.jpeg', 7.5);
