28f761d27c7ad20661a972c7fbfe7fd7ed444d97
Cocoricodai <colin.depelsenaire@gmail.com>
2022-11-28 09:13:53 +0100

    Beer db

---
+++ src/main/resources/db/migration/V2__dataBeer.sql
@@ -0,0 +1,2 @@
+insert into beers(nameBeer,url, degrees)  values ('Jupiler','http://jupiler.png', 5.2);
+insert into beers(nameBeer,url, degrees)  values ('Kaastelllll','http://Kaastelllll.png', 8);
