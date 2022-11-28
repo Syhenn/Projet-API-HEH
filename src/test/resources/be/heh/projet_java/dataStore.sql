ba6826b676c903b1843bbf4587716a14c308050a
Syhenn <sylvainhenquin10@gmail.com>
2022-11-28 09:15:25 +0100

    Store db test

---
+++ src/test/resources/be/heh/projet_java/dataStore.sql
@@ -0,0 +1,3 @@
+insert into stores(id_store,name,address)  values (10,'tata','tutu');
+insert into stores(id_store,name,address)  values (11,'tata1','tutu1');
+insert into stores(id_store,name,address)  values (12,'tata2','tutu2');
