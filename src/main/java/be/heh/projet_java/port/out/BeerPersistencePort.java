101bf9312d91dfa5260d673e44081d3cd8e6c5bf
Cocoricodai <colin.depelsenaire@gmail.com>
2022-11-28 09:10:49 +0100

    Beer port out

---
+++ src/main/java/be/heh/projet_java/port/out/BeerPersistencePort.java
@@ -0,0 +1,14 @@
+package be.heh.projet_java.port.out;
+
+import java.util.List;
+
+import be.heh.projet_java.adaptater.out.beer.BeerJpaEntity;
+import be.heh.projet_java.model.Beer;
+
+public interface BeerPersistencePort {
+    Beer addBeer(Beer beer);
+    void deleteBeerById(Long id);
+    Beer updateBeer(Beer beer);
+    List<Beer> getBeers();
+    Beer getBeerById(Long beer);
+}
