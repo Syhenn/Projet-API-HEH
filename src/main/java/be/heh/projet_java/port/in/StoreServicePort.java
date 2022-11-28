d23296903d79c12f89ff4ff98a20587285f3461f
Syhenn <sylvainhenquin10@gmail.com>
2022-11-28 09:11:02 +0100

    Store port in

---
+++ src/main/java/be/heh/projet_java/port/in/StoreServicePort.java
@@ -0,0 +1,18 @@
+package be.heh.projet_java.port.in;
+
+import be.heh.projet_java.model.Store;
+
+import java.util.List;
+
+public interface StoreServicePort {
+    Store addStore(Store store);
+
+    void deleteStoreById(Long id);
+
+    Store updateStore(Store store);
+
+    List<Store> getStores();
+
+    Store getStoreById(Long storeId);
+
+}
