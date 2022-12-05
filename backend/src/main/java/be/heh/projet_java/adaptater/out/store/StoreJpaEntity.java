2594a42dac46150ec93142fa064e4a4851e3662a
Cocoricodai <colin.depelsenaire@gmail.com>
2022-12-05 14:15:47 +0100

    Validation data

--- backend/src/main/java/be/heh/projet_java/adaptater/out/store/StoreJpaEntity.java
+++ backend/src/main/java/be/heh/projet_java/adaptater/out/store/StoreJpaEntity.java
@@ -3,6 +3,8 @@ package be.heh.projet_java.adaptater.out.store;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -18,9 +20,12 @@ public class StoreJpaEntity {
     private Long id;
 
     @Column(name = "name")
+    @NotNull(message = "Please provide a name")
+    @Size(min = 2, max = 128, message = "Name must be between 2 and 128")
     private String name;
 
     @Column(name = "address")
+    @NotNull(message = "Please provide a address")
     private String address;
 
 }
--- backend/src/main/java/be/heh/projet_java/adaptater/out/wine/WineJpaEntity.java
+++ backend/src/main/java/be/heh/projet_java/adaptater/out/wine/WineJpaEntity.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -20,11 +23,16 @@ public class WineJpaEntity {
     private Long id;
 
     @Column(name = "name")
+    @NotNull(message = "Please provide a name")
+    @Size(min = 2, max = 128, message = "Name must be between 2 and 128")
     private String name;
 
     @Column(name = "url")
+    @NotNull(message = "Please provide a url")
     private String url;
 
     @Column(name = "degrees")
+    @NotNull(message = "Please provide a degree")
+    @Positive(message = "Degree need to be positive")
     private float degrees;
 }
